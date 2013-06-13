74073bddd68c10c195e3d0a67eafd36dff0ff894
admin <admin@MULE109-018277.int.upao.edu.pe>
2013-06-13 11:48:52 -0500

    Arreglada clase Cliente (otra vez)

--- src/java/upao/paw/compumundo/modelo/Cliente.java
+++ src/java/upao/paw/compumundo/modelo/Cliente.java
@@ -4,7 +4,7 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable
-public class aaacliente {
+public class Cliente {
 
     @DatabaseField(generatedId = true)
     private int id;
