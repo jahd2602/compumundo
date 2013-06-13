ba976724844efa776b89618eee79fd6cf97ec989
admin <admin@MULE109-018277.int.upao.edu.pe>
2013-06-13 12:19:11 -0500

    Cambiada clase Cliente a Comprador

--- src/java/upao/paw/compumundo/modelo/Comprador.java
+++ src/java/upao/paw/compumundo/modelo/Comprador.java
@@ -4,7 +4,7 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable
-public class cliente {
+public class Comprador {
 
     @DatabaseField(generatedId = true)
     private int id;
