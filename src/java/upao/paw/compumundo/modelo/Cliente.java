31c5bf057b0e5e7185c5dfb64630d75548a40321
admin <admin@MULE109-018277.int.upao.edu.pe>
2013-06-13 12:10:05 -0500

    Arreglada clase Cliente por 3era vez

---
+++ src/java/upao/paw/compumundo/modelo/Cliente.java
@@ -0,0 +1,13 @@
+package upao.paw.compumundo.modelo;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable
+public class Cliente {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField
+    private String nombre;
+}
