93b8e5a9d5a970368d674a40dc5efd4fad962920
jahd <jahd@jahd-HP-Compaq-6710b>
2013-06-13 23:50:02 -0500

    Agregado atributo estado con constantes predefinidas

--- src/java/upao/paw/compumundo/modelo/Pedido.java
+++ src/java/upao/paw/compumundo/modelo/Pedido.java
@@ -6,10 +6,16 @@ import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable
 public class Pedido {
 
+    public static final int ESTADO_CARRITO = 0;
+    public static final int ESTADO_PEDIDO = 1;
+    public static final int ESTADO_CANCELADO = 2;
+    public static final int ESTADO_ENVIADO = 3;
     @DatabaseField(generatedId = true)
     private int id;
     @DatabaseField(foreign = true)
     private Comprador comprador;
+    @DatabaseField(defaultValue = "0") // ESTADO_CARRITO
+    private Integer estado;
 
     public int getId() {
         return id;
@@ -25,5 +31,13 @@ public class Pedido {
 
     public void setComprador(Comprador comprador) {
         this.comprador = comprador;
+    }
+
+    public Integer getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Integer estado) {
+        this.estado = estado;
     }
 }
